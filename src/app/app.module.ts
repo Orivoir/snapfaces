1eb90edbf6483aa28afa8fa004b36673fd2be0a8
Samuel Gaborieau <sam.gabor@hotmail.com>
2022-04-29 11:20:07 +0200

    upgrade app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from "@angular/platform-browser"
 
 import { AppComponent } from "./app.component"
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations"
+import {HeaderModule} from "./header/header.module"
+import { AppRoutingModule } from "./app-routing.module"
+import { HomeComponent } from "./home/home.component"
+import { MaterialModule } from "./material/material.module"
+import { CreateComponent } from "./create/create.component"
+import { SnapfacesComponent } from "./snapfaces/snapfaces.component"
+import { RouterModule } from "@angular/router"
+import { SingleSnapfaceComponent } from "./single-snapface/single-snapface.component"
+import { ReactiveFormsModule } from "@angular/forms"
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CreateComponent,
+    SnapfacesComponent,
+    SingleSnapfaceComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    HeaderModule,
+    AppRoutingModule,
+    MaterialModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
