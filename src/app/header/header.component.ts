6aadd42a41a49850d141171c153461ff5ae11d88
Samuel Gaborieau <sam.gabor@hotmail.com>
2022-04-29 11:14:57 +0200

    router navigation events at header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,19 +1,40 @@
-import { Component, Input } from "@angular/core"
-import { Router } from "@angular/router"
+import { Location } from "@angular/common"
+import { Component, Input, OnInit } from "@angular/core"
+import { NavigationEnd, Router, Event } from "@angular/router"
+import { filter } from "rxjs"
 
 @Component({
   selector: "app-header",
   templateUrl: "./header.component.html",
   styleUrls: ["./header.component.scss"]
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
 
   @Input() title: string = "hello world"
 
+  isCreateRoute: boolean = false
+
   constructor(private router: Router) { }
 
+  ngOnInit(): void {
+    // @TODO: add type check navigation event
+    // @TODO: memory leak observable at destroy component
+
+    const navigationEvent$ = this.router.events.pipe(
+      filter(routerEvent => routerEvent instanceof NavigationEnd),
+    )
+
+    navigationEvent$.subscribe((event: any) => {
+      this.isCreateRoute = event.urlAfterRedirects == "/snapfaces/create"
+    })
+  }
+
   onCreateSnapface(): void {
-    this.router.navigateByUrl("snapfaces/create")
+    if(!this.isCreateRoute) {
+      this.router.navigateByUrl("snapfaces/create")
+    } else {
+      this.router.navigateByUrl("")
+    }
   }
 
 }
